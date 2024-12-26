22120a0849eb34c3567a066cbbf1b2afd944e5a0
abdibrokhim <abdibrokhim@gmail.com>
2024-12-27 04:52:49 +0500

    added analytics, even tho i reached the limit, lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "./components/footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Analytics />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-3xl font-bold text-center mb-8">
           i built my 2024 wrapped with <span className="line-through">one shot</span> prompt using different llms
